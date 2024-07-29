c6c80b602d579dbc5b1423f579b3f848271b04ef
davidug <david@lugsports.com>
2024-07-29 12:30:52 -0400

    Re-added all pages and modified http address

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import { useState, useEffect } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import TeamCreate from './TeamCreate';
+import TeamCreate from './TeamCreate';
 import LeagueCreate from './LeagueCreate';
 import HomePage from './homePage';
  import DivisionCreate from './DivisionCreate';
  import LeagueEdit from './LeagueEdit';
-// import TeamsView from './TeamsView';
-// import TimeSlotCreate from './TimeslotCreate';
-// import TeamEdit from './TeamEdit';
-// import TimeslotEdit from './TimeslotEdit';
+import TeamsView from './TeamsView';
+import TimeSlotCreate from './TimeslotCreate';
+import TeamEdit from './TeamEdit';
+import TimeslotEdit from './TimeslotEdit';
 
 function App() {
   return (
@@ -21,14 +21,11 @@ function App() {
             <Route path='/league/create' element={<LeagueCreate />}></Route>
            <Route path='/league/:id/create' element={<DivisionCreate />}></Route>
            <Route path='/league/:id' element={<LeagueEdit />}></Route>
-           {
-          // <Route path="/league/:leagueID/division/:divisionID/teams" element={<TeamsView />} />
-          // <Route path = '/league/:leagueID/division/:divisionID/team' element={<TeamCreate/>}></Route>
-          // <Route path = '/league/:leagueID/division/:divisionID/timeslot' element={<TimeSlotCreate/>}></Route>
-          // <Route path = '/league/:leagueID/division/:divisionID/team/:teamID' element={<TeamEdit/>}></Route>
-          // <Route path = '/league/:leagueID/division/:divisionID/timeslot/:timeslotID' element={<TimeslotEdit/>}></Route> 
-           }
-          
+           <Route path="/league/:leagueID/division/:divisionID/teams" element={<TeamsView />} />
+           <Route path = '/league/:leagueID/division/:divisionID/team' element={<TeamCreate/>}></Route>
+           <Route path = '/league/:leagueID/division/:divisionID/timeslot' element={<TimeSlotCreate/>}></Route>
+           <Route path = '/league/:leagueID/division/:divisionID/team/:teamID' element={<TeamEdit/>}></Route>
+           <Route path = '/league/:leagueID/division/:divisionID/timeslot/:timeslotID' element={<TimeslotEdit/>}></Route>           
         </Routes>
       </BrowserRouter>
     </div>
